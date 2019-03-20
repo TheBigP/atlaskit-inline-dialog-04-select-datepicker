5a52bd7c4e7320b53c4b365c625ead62107efe3d
TheBig <2571019688@qq.com>
2019-03-20 16:43:54 +0800

    最初的版本

--- src/example.js
+++ src/example.js
@@ -6,27 +6,11 @@ import { DatePicker } from "@atlaskit/datetime-picker";
 import InlineDialog from "@atlaskit/inline-dialog";
 import { Checkbox } from "@atlaskit/checkbox";
 
-
 type State = {
   isDialogOpen: boolean
 };
 
 const options = [
-  { label: "Adelaide", value: "adelaide" },
-  { label: "Brisbane", value: "brisbane" },
-  { label: "Canberra", value: "canberra" },
-  { label: "Darwin", value: "darwin" },
-  { label: "Hobart", value: "hobart" },
-  { label: "Melbourne", value: "melbourne" },
-  { label: "Perth", value: "perth" },
-  { label: "Sydney", value: "sydney" },
-  { label: "Adelaide", value: "adelaide" },
-  { label: "Brisbane", value: "brisbane" },
-  { label: "Canberra", value: "canberra" },
-  { label: "Darwin", value: "darwin" },
-  { label: "Hobart", value: "hobart" },
-  { label: "Melbourne", value: "melbourne" },
-  { label: "Perth", value: "perth" },
   { label: "Adelaide", value: "adelaide" },
   { label: "Brisbane", value: "brisbane" },
   { label: "Canberra", value: "canberra" },
@@ -47,48 +31,22 @@ export default class SingleSelectDialog extends Component<{}, State> {
     this.setState({
       onChangeResult: `onChange called with value: ${
         event.target.value
-        } isChecked: ${event.target.checked}`
+      } isChecked: ${event.target.checked}`
     });
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.value;
-    console.log(target);
-    console.log("value +++++   " + value);
-    console.log("name +++++   " + name);
-    var partialState = {};
-    partialState[name] = value;
-    var new_test = name;
-    this.setState(new_test)
-    this.setState(partialState);
-    // this.setState({
-    //   [name]: value
-    // });
   };
   openDialog = () => {
     this.setState({ isDialogOpen: true });
   };
   dialogClosed = () => {
-    console.log(this.state.partialState);
     this.setState(prevState => ({ isDialogOpen: !prevState.isDialogOpen }));
   };
-  change_text = () => {
-    // console.log(this.state.is_checked);
-    console.log(this.state.new_test);
-  }
 
   render() {
     const content = (
       <div style={{ width: "300px" }}>
-        <div>
-          <h1>Columns</h1>
-        </div>
-        <div>
-          <input placeholder="Search" type="text" onChange={this.change_text} ref="search" />
-        </div>
-        <p style={{ overflow: 'scroll' , height: "200px"}}>
+        <p>
           {options.map(v => (
             <Checkbox
-              id="checkbox"
               value={v.value}
               label={v.label}
               onChange={this.onChange}
